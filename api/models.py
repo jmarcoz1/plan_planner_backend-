de9622d9f9b1ef7753852d034ba3b670d64b02a2
Jorge Marco <jmararr1@gmail.com>
2024-01-07 18:42:09 +0100

    changes

--- api/models.py
+++ api/models.py
@@ -8,9 +8,13 @@ class TimeOfDay(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='parentcategories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Activity(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='activities')
     time_of_day = models.ManyToManyField(TimeOfDay)
--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers
 from .models import Category, Activity, TimeOfDay
 
 class CategorySerializer(serializers.ModelSerializer):
+    parentcategories = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Category.objects.all(),
+        required=False
+    )
+
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['name', 'parentcategories']
+
+    def create(self, validated_data):
+        parent_name = validated_data.pop('parentcategories', None)
+        parent_category = None
+        if parent_name:
+            parent_category = Category.objects.get(name=parent_name)
+        return Category.objects.create(parent=parent_category, **validated_data)
 
 class ActivitySerializer(serializers.ModelSerializer):
     time_of_day = serializers.SlugRelatedField(
@@ -21,9 +34,14 @@ class ActivitySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         time_of_day_data = validated_data.pop('time_of_day')
         category_data = validated_data.pop('category')
-        category, _ = Category.objects.get_or_create(name=category_data['name'])
+        parent_name = category_data.pop('parentcategories', None)
+        parent_category = None
+        if parent_name:
+            parent_category, _ = Category.objects.get_or_create(name=parent_name)
+        category, _ = Category.objects.get_or_create(name=category_data['name'], parent=parent_category)
         activity = Activity.objects.create(category=category, **validated_data)
         time_of_day_set = [TimeOfDay.objects.get_or_create(name=name)[0] for name in time_of_day_data]
         activity.time_of_day.set(time_of_day_set)
         return activity
+
 
